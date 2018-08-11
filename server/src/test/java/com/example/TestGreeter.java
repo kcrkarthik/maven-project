1d5cd5cc144de9ed4485dc47cebf9d7b1436f5db
kcrkarthik <kcrkarthik@users.noreply.github.com>
2018-08-11 06:57:22 +0530

    Update TestGreeter.java

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -2,7 +2,6 @@ package com.example;
 
 import org.junit.Before;
 import org.junit.Test;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertThat;
@@ -24,6 +23,7 @@ public class TestGreeter {
     assertThat(greeter.greet(someone), containsString(someone));
   }
 
+  
   @Test
   public void greetShouldIncludeGreetingPhrase() {
     String someone = "World";
